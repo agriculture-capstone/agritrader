96661ccd94f1c119c68d11543bcfafd0a5232894
James Inglis <jamesekinglis@gmail.com>
2017-11-08 17:24:37 -0700

    got rid of everything ... because it was wrong

--- src/index.tsx
+++ src/index.tsx
@@ -1,37 +1,4 @@
-import React, { Component } from 'react';  
-import { 
-    AppRegistry,
-    Text,
-    View,
-    Navigator
-} from 'react-native';
+import { AppRegistry } from 'react-native';
 import App from './App';
 
 AppRegistry.registerComponent('agritrader', () => App);
-
-class ToolbarNav extends Component {
-
-  _renderScene (route, navigator) {
-    switch (route.id) {
-      case 'App':
-        return (
-          <App navigator={navigator}/>
-        )
-      case 'settings':
-        return (
-          <Text>Settings</Text>
-        )
-    }
-  }
-
-  render() {
-    return (
-      <Navigator
-        initialRoute={{ id: 'App' }}
-        renderScene={(route, navigator) => this._renderScene(route, navigator)}
-      />
-    );
-  }
-}
-
-AppRegistry.registerComponent('ToolbarNav', () => ToolbarNav);  
