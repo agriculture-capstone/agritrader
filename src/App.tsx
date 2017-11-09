a0c6344110bb755c370da610b71be40fe5ccd00d
James Inglis <jamesekinglis@gmail.com>
2017-11-09 06:59:38 -0700

    custom Toolbar Component is close to implemented the only remaining part is to get icons or the right buttons implemented

--- src/App.tsx
+++ src/App.tsx
@@ -9,33 +9,35 @@ import {
   Platform,
   StyleSheet,
   Text,
-  View
+  View,
+  ToolbarAndroid
 } from 'react-native';
+import Toolbar, { LeftButtonTypes} from './Components/Toolbar'
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
+  'Cmd+D or shake for dev menu',
   android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+  'Shake or press menu button for dev menu',
 });
 
 export default class App extends React.Component<{}, {}> {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Hello! This is the agritrader app!!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
-    );
+      <Toolbar 
+        title='Agritrader'
+        rightButtons={[
+          {title: 'button1', icon:'../src/Assets/account_white_24dp.png', 
+          action: () => {console.log("Right Button1 Action")}},
+          {title: 'button2', icon:'../src/Assets/settings_white_24dp.png', 
+          action: () => {console.log("Right Button2 Action")}}
+        ]}
+        leftButtonType={LeftButtonTypes.menu}
+        />
+    )
   }
 }
+
 
 const styles = StyleSheet.create({
   container: {
@@ -53,5 +55,12 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     color: '#333333',
     marginBottom: 5,
+  },
+  parentContainer: {
+      flex: 1,
+  },
+  toolbar: {
+      height: 56,
+      backgroundColor: '#4883da',
   },
 });
---
+++ src/Components/Toolbar.tsx
@@ -0,0 +1,60 @@
+import {
+    StyleSheet,
+    ToolbarAndroid,
+    ToolbarAndroidAction
+} from 'react-native';
+import * as React from 'react';
+
+export enum LeftButtonTypes {
+    menu = "menu",
+    back = "back",
+}
+
+interface RightButtonType {
+    title: string;
+    icon?: string;
+    action: () => void;
+}
+interface ToolbarProps {
+    title: string;
+    leftButtonType: LeftButtonTypes;
+    rightButtons: Array<RightButtonType>;
+}
+
+export default class Toolbar extends React.Component<ToolbarProps, {}>  {
+    constructor(props: ToolbarProps) {
+        super(props);
+    }
+    onActionSelected = (position: number) => {
+        this.props.rightButtons[position].action();
+    }
+    render() {
+        let actionArray = new Array<ToolbarAndroidAction>();
+        this.props.rightButtons.map(RightButton => {
+            let action: ToolbarAndroidAction = { title: RightButton.title, show: 'always',}            
+            if(RightButton.icon){
+                action.icon = require(RightButton.icon)
+            }
+            actionArray.push(action)
+        })
+        return (
+            <ToolbarAndroid
+                title={this.props.title}
+                titleColor='white'
+                style={styles.toolbar}
+                navIcon={(this.props.leftButtonType == "menu") ? require('../../src/Assets/menu_white_24dp.png') 
+                : require('../../src/Assets/back_white_24dp.png')}
+                onIconClicked={(this.props.leftButtonType == "menu") ? () => console.log("menu icon selected") 
+                : () => console.log("back icon selected")}
+                onActionSelected={this.onActionSelected}
+                actions={actionArray} />
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    toolbar: {
+        height: 56,
+        backgroundColor: '#4883da',
+    },
+});
