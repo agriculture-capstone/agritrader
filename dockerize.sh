84b77adbc5dd5d564360eb5fcfd845f228fbd77b
Enoch Tsang <etsang1@hotmail.com>
2017-11-18 02:29:44 -0700

    at-23 clean up utility docker script

--- dockerize.sh
+++ dockerize.sh
@@ -3,31 +3,29 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 IMAGE_NAME="boresha/agritrader"
 DOCKER_HOME="/agritrader"
-ARG_NUM=2
 
 if [ "$1" == "init" ]; then
-	docker build -t $IMAGE_NAME $DIR
+	docker build -t "$IMAGE_NAME" "$DIR"
 elif [ "$1" == "install" ]; then
-<<<<<<< HEAD
 	docker run --rm \
-		-v $DIR:$DOCKER_HOME \
-		$IMAGE_NAME yarn install
+		-v "$DIR":"$DOCKER_HOME" \
+		"$IMAGE_NAME" yarn install
 elif [ "$1" == "adb" ]; then
 	# Useful to check if docker image can detect your phone at all
 	docker run --rm \
-		-v $DIR:$DOCKER_HOME \
-		--device=${@:2} \
-		$IMAGE_NAME adb devices
+		-v "$DIR":"$DOCKER_HOME" \
+		--device="$2" \
+		"$IMAGE_NAME" adb devices
 elif [ "$1" == "android" ]; then
 	docker run --rm \
-		-v $DIR:$DOCKER_HOME \
-		--device=${@:2} \
-		-t $IMAGE_NAME ./start-android.sh
+		-v "$DIR":"$DOCKER_HOME" \
+		--device="$2" \
+		-t "$IMAGE_NAME" ./start-android.sh
 elif [ "$1" == "run" ]; then
 	docker run --rm \
-		-v $DIR:$DOCKER_HOME \
-		$IMAGE_NAME npm run "${@:2}"
+		-v "$DIR":"$DOCKER_HOME" \
+		"$IMAGE_NAME" npm run "${@:2}"
 else
-	echo "usage: dockerize.sh [init|install|adb|android|run] "
+	echo "usage: dockerize.sh [init | install | adb [device] | android [device] | run [script] ] "
 fi
 
