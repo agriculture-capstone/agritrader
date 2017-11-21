2b3ce5c845ae002f51a7c6d520a1235296ab762d
James Inglis <jamesekinglis@gmail.com>
2017-11-21 11:08:31 -0700

    setup enzyme test to check rendered objects

--- src/__tests__/components/Toolbar.enzyme.test.tsx
+++ src/__tests__/components/Toolbar.enzyme.test.tsx
@@ -1,12 +1,15 @@
 // __tests__/Toolbar.enzyme.test.tsx
 
+import assets from '../assets';
+import Toolbar, {
+  LeftButtonTypes,    
+} from '../components/Toolbar';
 import react from 'react';
 import { shallow } from 'enzyme';
 import CheckboxWithLabel from '../CheckboxWithLabel';
 
-test('CheckboxWithLabel changes the text after click', () => {
-  // Render a checkbox with label in the document
-  const checkbox = shallow(
+test('test that ', () => {
+  const toolbar = shallow(
     <Toolbar 
         title="Agritrader"
         rightButtons={
@@ -31,9 +34,10 @@ test('CheckboxWithLabel changes the text after click', () => {
         }
     />);
 
-  expect(checkbox.text()).toEqual('Off');
-
-  checkbox.find('input').simulate('change');
-
-  expect(checkbox.text()).toEqual('On');
+  expect(toolbar.title).toEqual('Agritrader');
+  expect(toolbar.rightButtons[0].title).toEqual('button1');
+  expect(toolbar.rightButtons[0].icon).toEqual(assets.account);
+  expect(toolbar.rightButtons[1].title).toEqual('button2');
+  expect(toolbar.rightButtons[1].icon).toEqual(assets.settings);
+  expect(toolbar.leftButtonTypes).toEqual(LeftButtonTypes.menu);
 });
