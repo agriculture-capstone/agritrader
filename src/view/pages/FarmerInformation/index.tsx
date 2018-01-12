dbabd8a84450406175c84eb76e1647f9ee2be770
Moath <mfalthaw@ucalgary.ca>
2018-01-11 22:11:56 -0700

    AT-47 edit farmer styling

--- src/view/pages/FarmerInformation/index.tsx
+++ src/view/pages/FarmerInformation/index.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Container, Header, Content, Form, Item, Input, Label, Radio, Right, ListItem, InputGroup, Picker } from 'native-base';
+import { Container, Header, Content, Grid, Row, Col, Form, Item, Input, Label, Radio, Right, ListItem, InputGroup, Picker } from 'native-base';
 import { View, Text } from 'react-native';
 
 /**
@@ -13,55 +13,80 @@ export default class FarmerInformation extends React.Component<{}, {}> {
     return (
         <Content>
           <Form>
-            <Item floatingLabel>
-              <Label>First Name</Label>
-              <Input />
-            </Item>
-            
-            <Item floatingLabel>
-              <Label>Last Name</Label>
-              <Input />
-            </Item>
-            
-            <Item floatingLabel>
-              <Label>Phone Number</Label>
-              <Input />
-            </Item>
+           
+            <Grid>
+              <Row>
+                <Col>
+                    <Item floatingLabel>
+                      <Label>First Name</Label>
+                      <Input />
+                    </Item>
+                </Col>
+                <Col>
+                    <Item floatingLabel>
+                      <Label>Last Name</Label>
+                      <Input />
+                    </Item>
+                </Col>
+              </Row>
 
-            <Item>
-              <Label>Payment Method</Label>
-            </Item>
-            <ListItem>
-                  <Text>Mobile</Text>
-                  <Right>
-                    <Radio selected={false} />
-                  </Right>
-                </ListItem>
-                <ListItem>
-                  <Text>Cash</Text>
-                  <Right>
-                    <Radio selected={true} />
-                  </Right>
-            </ListItem>
-              
-            <Item>
-              <Label>Payment Cycle</Label>
-            </Item>
-            <Picker
-              iosHeader="Select one"
-              mode="dropdown"
-              selectedValue={() => {}}
-              onValueChange={() => {}}
-            >
-              <Item label="Weekly" value="key0" />
-              <Item label="Bi-Weekly" value="key1" />
-              <Item label="Monthly" value="key2" />
-            </Picker>
-            
-            <Item floatingLabel>
-              <Label>Notes</Label>
-              <Input />
-            </Item>
+              <Row>
+                <Col>
+                    <Item floatingLabel>
+                      <Label>Phone Number</Label>
+                      <Input />
+                    </Item>
+                </Col>
+              </Row>
+
+              <Row>
+                <Col>
+                    <Item floatingLabel>
+                      <Label>Business Name</Label>
+                      <Input />
+                    </Item>
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                  <Item floatingLabel>
+                    <Label>Payment Cycle</Label>
+                  </Item>
+                  <Picker
+                    iosHeader="Select one"
+                    mode="dropdown"
+                    selectedValue={() => {}}
+                    onValueChange={() => {}}
+                  >
+                    <Item label="Weekly" value="key0" />
+                    <Item label="Bi-Weekly" value="key1" />
+                    <Item label="Monthly" value="key2" />
+                  </Picker>
+                </Col>
+                <Col>
+                  <Item floatingLabel>
+                    <Label>Payment Method</Label>
+                  </Item>
+                  <Picker
+                    iosHeader="Select one"
+                    mode="dropdown"
+                    selectedValue={() => {}}
+                    onValueChange={() => {}}
+                  >
+                    <Item label="Mobile" value="key0" />
+                    <Item label="Cash" value="key1" />
+                  </Picker>
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                    <Item floatingLabel>
+                      <Label>Notes</Label>
+                      <Input />
+                    </Item>
+                </Col>
+              </Row>
+            </Grid> 
           </Form>
         </Content>
     );
