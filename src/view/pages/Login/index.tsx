d69c86b140dfe35bc191b2dee0357abb02864ae1
James Inglis <jamesekinglis@gmail.com>
2018-01-16 16:19:50 -0700

    Using basic Native base components

--- src/view/pages/Login/index.tsx
+++ src/view/pages/Login/index.tsx
@@ -1,29 +1,53 @@
 import * as React from 'react';
+import {
+  Image,
+  View,
+} from 'react-native';
+import {
+  Container,
+  Header,
+  Content,
+  Form,
+  Item,
+  Input,
+  Label,
+  Button,
+  Text,
+  Right,
+  Grid,
+  Row,
+  Col,
+  Icon,
+} from 'native-base';
 import { icons } from '../../assets/';
 import { styles } from './style';
-import Logo from './components/Logo';
-import Form from './components/Form';
-import Wallpaper from './components/Wallpaper';
-import ButtonSubmit from './components/ButtonSubmit';
-import ForgotPassword from './components/ForgotPassword';
-import {
-  StyleSheet,
-  KeyboardAvoidingView,
-  View,
-  ActivityIndicator,
-  TouchableOpacity,
-  Image,
-} from 'react-native';
 
 export default class Login extends React.Component {
   public render() {
+    const logoSize = 100;
     return (
-        <Wallpaper>
-          <Logo/>
-          <Form/>
-          <ButtonSubmit/>
-          <ForgotPassword/>
-        </Wallpaper>
+      <Container>
+        <Content padder>
+          <Form>
+            <Grid>
+              <Row>
+                <Image style={{ height: 200, width: 200 }} source={icons.logo} />
+              </Row>
+              <Item floatingLabel >
+                <Label >Username</Label>
+                <Input />
+              </Item>
+              <Item floatingLabel>
+                <Label >Password</Label>
+                <Input />
+              </Item>
+              <Button block primary >
+                <Text >Login</Text>
+              </Button>
+            </Grid>
+          </Form>
+        </Content>
+      </Container >
     );
   }
 }
--- src/view/pages/Login/style.ts
+++ src/view/pages/Login/style.ts
@@ -1,104 +1,45 @@
+
 const React = require('react-native');
 const { Platform, Dimensions, StyleSheet } = React;
 
-export const deviceHeight = Dimensions.get('window').height;
 export const deviceWidth = Dimensions.get('window').width;
+export const deviceHeight = Dimensions.get('window').height;
+
 
 export const styles = StyleSheet.create({
-  /*Logo styling*/
   container: {
-    flex: 2,
-    top: 25,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-
-  image: {
-    width: 200,
-    height: 200,
-  },
-
-  text: {
-    color: 'white',
-    fontWeight: 'bold',
-    backgroundColor: 'transparent',
-  },
-
-  /* wallpaper styling */
-  wallpaper: {
-    flex: 1,
     backgroundColor: '#0fc6ce',
   },
-  /* ButtonSubmit styling */
-  submitContainer: {
-    flex: 1,
-    bottom: 20,
-    alignItems: 'center',
+  centerContent: {
     justifyContent: 'center',
+    alignItems: 'center',
   },
-  button: {
-    alignItems: 'center',
-    justifyContent: 'center',
+  logo: {
+    alignSelf: 'stretch',
+    height: deviceHeight / 3.5,
+    width: deviceHeight / 3.5,
+    position: 'relative',
+    marginBottom: 10,
+  },
+  textFields: {
+    width: deviceWidth/1.25,
+    left: -8,
+  },
+  
+  forgotPassword: {
+    paddingLeft: deviceWidth/1.8,
+  },
+  loginButton: {
+    width: deviceWidth/1.3,
     backgroundColor: 'white',
-    height: 40,
-    width: 150,
-    borderRadius: 40,
-  },
-  submittext: {
-    color: 'white',
-    backgroundColor: 'transparent',
-  },
-  submitImage: {
-    width: 100,
-    height: 100,
+    top: 10 ,
   },
 
-  /*ForgotPassword styling*/
-  forgotPasswordContainer: {
-    flex: 1,
-    left: 210,
-    bottom: 25,
+  buttonText: {
+    color: 'black',
   },
-  forgotPasswordText: {
+  labelText: {
     color: 'white',
-    backgroundColor: 'transparent',
-  },
-
-  /*Form styling*/
-  formContainer: {
-    flex: 1,
-    alignItems: 'center',
-    paddingTop: 20,
-  },
-  btnEye: {
-    position: 'absolute',
-    top: 55,
-    right: 28,
-  },
-  iconEye: {
-    width: 25,
-    height: 25,
-    tintColor: 'rgba(0,0,0,0.2)',
-  },
-  /*UserInput styling*/
-  input: {
-    backgroundColor: 'rgba(255, 255, 255, 0.4)',
-    width: deviceWidth - 40,
-    height: 40,
-    marginHorizontal: 20,
-    paddingLeft: 45,
-    borderRadius: 20,
-    color: '#ffffff',
-  },
-  inputWrapper: {
-    flex: 1,
-  },
-  inlineImg: {
-    position: 'absolute',
-    zIndex: 99,
-    width: 22,
-    height: 22,
-    left: 35,
-    top: 9,
+    paddingLeft: 8,
   },
 });
