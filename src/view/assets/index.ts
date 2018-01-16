5bd835fc4fe35ab131a3b6d62183365cc115f7fc
James Inglis <jamesekinglis@gmail.com>
2018-01-16 16:22:45 -0700

    fixed asset issue

--- src/view/assets/index.ts
+++ src/view/assets/index.ts
@@ -1,7 +1,3 @@
 export const icons = {
-  logo: require('../../../src/view/assets/boresha_circular_logo.png'),
-  username: require('../../../src/view/assets/username.png'),
-  password: require('../../../src/view/assets/password.png'),
-  loading: require('../../../src/view/assets/loading.gif'),    
-    
+  logo: require('../../../src/view/assets/boresha_circular_logo.png'),    
 };
