5d60a930a3a921f6165221edf171919f4e95a9fa
James Inglis <jamesekinglis@gmail.com>
2018-01-17 14:41:43 -0700

    inputs now write to variables and login button has an onpress function

--- src/view/pages/Login/index.tsx
+++ src/view/pages/Login/index.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import {
   Image,
+  TouchableHighlight,
 } from 'react-native';
 import {
   Container,
@@ -18,7 +19,28 @@ import {
 import { images } from '../../assets/';
 import { styles } from './style';
 
-export default class Login extends React.Component {
+
+
+export default class Login extends React.Component<{}, {}>{
+
+  public state = {
+    username: '',
+    password: '',
+  };
+
+  private updateUsername = (value: string) => {
+    this.setState({ username: value });
+  }
+
+  private updatePassword = (value: string) => {
+    this.setState({ password: value });
+  }
+
+  private loginPress = () => {
+    alert(`Login Pressed - username:${this.state.username} password:${this.state.password}`);
+    //
+  }
+
   public render() {
     const logoSize = 100;
     return (
@@ -34,22 +56,27 @@ export default class Login extends React.Component {
               <Row>
                 <Col>
                   <Item floatingLabel style={styles.label} >
-                    <Label style={styles.labelText}>Username</Label>
-                    <Input />
+                    <Label style={{ color: 'white', paddingLeft: 8 }}>Username</Label>
+                    <Input onChangeText={this.updateUsername} />
                   </Item>
                 </Col>
               </Row>
               <Row>
                 <Col>
                   <Item floatingLabel style={styles.label}>
-                    <Label style={styles.labelText}>Password</Label>
-                    <Input />
+                    <Label style={{ color: 'white', paddingLeft: 8 }}>Password</Label>
+                    <Input onChangeText={this.updatePassword} secureTextEntry={true} />
                   </Item>
                 </Col>
               </Row>
               <Row style={styles.buttonRow}>
                 <Col>
-                  <Button block primary style={styles.loginButton}>
+                  <Button
+                    block
+                    primary
+                    style={styles.loginButton}
+                    onPress={this.loginPress}
+                  >
                     <Text style={styles.buttonText}>Login</Text>
                   </Button>
                 </Col>
