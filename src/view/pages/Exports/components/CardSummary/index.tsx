a46d099eed4bd711d68eb2a000d8d66d46706508
James Inglis <jamesekinglis@gmail.com>
2018-01-17 21:55:50 -0700

    started modifying the collection page to work for exports

---
+++ src/view/pages/Exports/components/CardSummary/index.tsx
@@ -0,0 +1,63 @@
+import * as React from 'react';
+import { Root, Grid, Row, Col } from 'native-base';
+import { Text, StyleSheet } from 'react-native';
+import { Provider } from 'react-redux';
+
+import Panel from '../../../../components/Panel';
+
+interface CardSummaryPropsType {
+  title: string;
+  currentWeekTotal: string;
+  currentMonthTotal: string;
+  allTimeTotal: string;
+}
+
+/**
+ * Container for CardSummary
+ */
+export default class CardSummary extends React.Component<CardSummaryPropsType, {}> {
+  /**
+   * Render method for CardSummary
+   */
+  public render() {
+    return (
+      <Panel title={this.props.title}>
+        <Grid >
+          <Row>
+            <Col>
+              <Text style={styles.info}>
+                <Text style={styles.label}>This Week: </Text>
+                {this.props.currentWeekTotal}
+              </Text>
+            </Col>
+            <Col>
+              <Text style={styles.info}>
+                <Text style={styles.label}>This Month: </Text>
+                {this.props.currentMonthTotal}
+              </Text>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <Text style={styles.info}>
+                <Text style={styles.label}>All-Time Total Amount: </Text>
+                {this.props.allTimeTotal}
+              </Text>
+            </Col>
+          </Row>
+        </Grid>
+      </Panel>
+    );
+  }
+}
+
+
+const styles = StyleSheet.create({
+  info: {
+    textAlign: 'center',
+    padding: 5,
+  },
+  label: {
+    fontWeight: 'bold',
+  },
+});
---
+++ src/view/pages/Exports/index.tsx
@@ -0,0 +1,28 @@
+import * as React from 'react';
+import { Root, Grid, Row, Col } from 'native-base';
+import { Text, StyleSheet } from 'react-native';
+import { Provider } from 'react-redux';
+
+import ProductCard from './components/ProductCard';
+import Collect from './tabs/Collect';
+
+/**
+ * Container for Farmer
+ */
+export default class Farmer extends React.Component<{}, {}> {
+  /**
+   * Render method for Farmer
+   */
+  public render() {
+    // TODO based on the active tab, we will decide which tab to render
+    return (
+      <Collect
+        farmerName="Bradley the Farmer"
+        allTimeTotal="6049.L"
+        currentWeekTotal="60.L"
+        currentMonthTotal="180L"
+        collectionValues={[{ date: 'Jan 1', am: 2, pm: 2 }, { date: 'Jan 1', am: 2, pm: 2 }]}
+      />
+    );
+  }
+}
