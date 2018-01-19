c2ef32092658dfeeb7920377a13a09d694b42d46
Bea Esguerra <bea.esguerra@hotmail.com>
2018-01-19 00:51:38 -0700

    AT-54 leaving log out button at the end of the list

--- src/view/components/Drawer/DrawerContents/index.tsx
+++ src/view/components/Drawer/DrawerContents/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { Grid, Row } from 'react-native-easy-grid';
-import { Content, List, Button, Text } from 'native-base';
+import { Content, List, Button, Text, ListItem } from 'native-base';
 import { View } from 'react-native';
 import { routes, RouteInfo } from '../../../navigation/navigator';
 import style from './style';
@@ -14,14 +14,12 @@ const DrawerContents: React.StatelessComponent<OwnProps> = (props) => {
   return (
     <Content style={style.content} >
       <DrawerHeader />
-      <List style={{height: 470}}>
+      <List>
         {getDrawerItems(routes)}
       </List>
-      <View>
       <Button danger full>
       <Text> Sign out </Text>
       </Button>
-      </View>
     </Content>
   );
 };
