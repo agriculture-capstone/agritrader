470082bc1372a66e1dea31afa9c493da6e8ff482
Bea Esguerra <bea.esguerra@hotmail.com>
2018-01-21 17:38:44 -0700

    cleaning up card button and enabling tsconfig rule for unused parameters

--- src/view/components/CardButton/index.tsx
+++ src/view/components/CardButton/index.tsx
@@ -1,8 +1,7 @@
 import * as React from 'react';
-import { Root, Grid, Row, Col, Content, Button, Text, Card, Body, CardItem, Icon, Right, Left } from 'native-base';
-import { StyleSheet, Alert } from 'react-native';
+import { Text, Card, Body, CardItem, Icon, Right, Left } from 'native-base';
 import { Route } from '../../navigation/navigator';
-
+import styles from './style';
 
 interface CardButtonPropsType {
   title: string;
@@ -11,13 +10,13 @@ interface CardButtonPropsType {
   route: Route;
   onPress(route: Route): void;
 }
+
 /**
- * Home Page Component
- */
+* Card Button Component
+*/
 export default class CardButton extends React.Component<CardButtonPropsType, {}> {
   public constructor(props: CardButtonPropsType) {
     super(props);
-
     this.onPress = this.onPress.bind(this);
   }
 
@@ -25,48 +24,33 @@ export default class CardButton extends React.Component<CardButtonPropsType, {}>
     this.props.onPress(this.props.route);
   }
 
-  // TODO need to connect this to the redux state
-    /**
-     * Render method for Farmer
-     */
-    public render() {
-        return (
+  /**
+   * Render method 
+   */
+  public render() {
+    const iconColor = {
+      color: this.props.iconColor,
+    };
 
-            <Card >
-                <CardItem button onPress={this.onPress}>
-
-                    <Left >
-                        <Icon
-                            style={{
-                                fontSize: 40,
-                                color: this.props.iconColor
-                            }}
-                            name={this.props.iconName}
-                        />
-
-                    <Body>
-                        <Text>
-                            {this.props.title}
-                        </Text>
-                    </Body>
-                    </Left>
-                    <Right>
-                        <Icon name="ios-arrow-forward" />
-                    </Right>
-
-                </CardItem>
-            </Card>
-
-        );
-    }
+    return (
+      <Card>
+        <CardItem button onPress={this.onPress}>
+          <Left>
+            <Icon
+              style={[styles.icon, iconColor]}
+              name={this.props.iconName}
+            />
+            <Body>
+              <Text>
+                {this.props.title}
+              </Text>
+            </Body>
+          </Left>
+          <Right>
+            <Icon name="ios-arrow-forward" />
+          </Right>
+        </CardItem>
+      </Card>
+    );
+  }
 }
-
-
-let styles = StyleSheet.create({
-    arrow: {
-        alignSelf: "flex-end",
-    },
-    card: {
-        padding: 5
-    }
-});
