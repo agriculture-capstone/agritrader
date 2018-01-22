1169a4609b165b577a4411eb650d7f192cec621e
Bea Esguerra <bea.esguerra@hotmail.com>
2018-01-21 18:33:57 -0700

    clean datatable

--- src/view/components/DataTable/index.tsx
+++ src/view/components/DataTable/index.tsx
@@ -1,53 +1,76 @@
 import * as React from 'react';
 import { List, ListItem, Text, Grid, Col } from 'native-base';
 
-interface DataTablePropsType {
-  headers: Array<string>;
-  values: Array<any>;
+
+interface OwnPropsType {
+  headers: string[];
+  values: any[];
 }
+
+interface DispatchPropsType {
+
+}
+
+interface StorePropsType {
+
+}
+
+type PropsType = OwnPropsType & DispatchPropsType & StorePropsType;
+
+interface OwnStateType {
+
+}
+
 /**
-* Container for DataTable
+* Component for DataTable
 */
-export default class DataTable extends React.Component<DataTablePropsType, {}> {
-  
-  public renderRow(item: any) {
-    const keys = Object.keys(item); 
-    return (
-            <ListItem>
-              <Grid>
-                {keys.map((x) => {
-                  return (<Col><Text>{item[x]}</Text></Col>);})}
-                </Grid>
-            </ListItem>
-    );
+export default class DataTable extends React.Component<PropsType, OwnStateType> {
+  constructor(props: PropsType) {
+    super(props);
+    this.renderSectionHeader.bind(this);
   }
 
-
-  public renderSectionHeader(sectionData: any, sectionId: any) {
+  private formatValues(values: any[]) {
+    return values.map((value) => {
+      return (
+        <Col key={value}>
+          <Text>
+            {value}
+          </Text>
+        </Col>
+      );
+    });
+  }
+  
+  private renderRow(item: any) {
     return (
-      <ListItem itemHeader first>
+      <ListItem>
         <Grid>
-          {
-            this.props.headers.map((x) => { 
-              return <Col><Text>{x}</Text></Col>;
-            })
-          }
+          {this.formatValues(item.values())}
         </Grid>
       </ListItem>
     );
   }
 
-  
+  private renderSectionHeader() {
+    return (
+      <ListItem itemHeader first>
+        <Grid>
+          {this.formatValues(this.props.headers)}
+        </Grid>
+      </ListItem>
+    );
+  }
+
   /**
   * Render method for DataTable
   */
   public render() {
     return (
-      <List 
+      <List
         dataArray={this.props.values}
         renderRow={this.renderRow}
-        renderSectionHeader={this.renderSectionHeader.bind(this)}
-        
+        renderSectionHeader={this.renderSectionHeader}
       />
     );
   }
