0d28a2d0e815c32ab3061bedc116b8bf2c2b4e23
Brad <bradpf@outlook.com>
2018-01-23 16:34:56 -0700

    AT-100 Fixing fuse filtering

--- src/view/pages/FarmerSearch/index.tsx
+++ src/view/pages/FarmerSearch/index.tsx
@@ -10,24 +10,73 @@ import navActions from '../../../store/modules/nav/actions';
 import { Route } from '../../navigation/navigator';
 import { State } from '../../../store/types';
 import style from './style';
-import * as Fuse from 'fuse.js'
+import * as Fuse from 'fuse.js';
+import { FuseOptions } from 'fuse.js';
 
 /** This is just a table of phony information to populate the FarmerSearch UI */
-const defaultFarmerList = [{ name: 'Swalleh', phoneNumber: '1-250-234-1234', id: 1 },
-                    { name: 'James', phoneNumber: '1-526-123-8123', id: 2 },
-                    { name: 'Alex', phoneNumber: '1-514-235-6789', id: 3 },
-                    { name: 'Joseph', phoneNumber: '1-922-789-2348', id: 4 },
-                    { name: 'Mary', phoneNumber: '1-626-626-1236', id: 5 },
-                    { name: 'David', phoneNumber: '1-789-231-2345', id: 6 },
-                    { name: 'Michael', phoneNumber: '1-899-781-8786', id: 7 },
-                    { name: 'Mary', phoneNumber: '1-897-768-6780', id: 8 },
-                    { name: 'Peter', phoneNumber: '1-123-564-2315', id: 9 },
-                    { name: 'Jonah', phoneNumber: '1-011-101-1001', id: 10 },
-                    { name: 'Simon', phoneNumber: '1-234-456-7890', id: 11 }];
+const farmerList: Farmer[] = [
+  {
+    name: 'Swalleh',
+    phoneNumber: '1-250-234-1234',
+    id: 1,
+  },
+  {
+    name: 'James',
+    phoneNumber: '1-526-123-8123',
+    id: 2,
+  },
+  {
+    name: 'Alex',
+    phoneNumber: '1-514-235-6789',
+    id: 3,
+  },
+  {
+    name: 'Joseph',
+    phoneNumber: '1-922-789-2348',
+    id: 4,
+  },
+  {
+    name: 'Mary',
+    phoneNumber: '1-626-626-1236',
+    id: 5,
+  },
+  {
+    name: 'David',
+    phoneNumber: '1-789-231-2345',
+    id: 6,
+  },
+  {
+    name: 'Michael',
+    phoneNumber: '1-899-781-8786',
+    id: 7,
+  },
+  {
+    name: 'Mary',
+    phoneNumber: '1-897-768-6780',
+    id: 8,
+  },
+  {
+    name: 'Peter',
+    phoneNumber: '1-123-564-2315',
+    id: 9,
+  },
+  {
+    name: 'Jonah',
+    phoneNumber: '1-011-101-1001',
+    id: 10,
+  },
+  {
+    name: 'Simon',
+    phoneNumber: '1-234-456-7890',
+    id: 11,
+  },
+];
 
 /** Basic model for the FarmerType object */
 // TODO: Move to store --@jinglis
-interface FarmerType {
+// TODO: should match Core API for farmer @nick
+// TODO: Phones should be shown (123) 456-7890 @nick
+interface Farmer {
   name: string;
   phoneNumber: string;
   id: number;
@@ -36,11 +85,11 @@ interface FarmerType {
 /** FarmerSearch OwnPropsType */
 // TODO: Make required property when moving to StorePropsType
 export interface OwnPropsType {
-  listItems?: FarmerType[];
+
 }
 
 /** FarmerSearch StorePropsType */
-interface StorePropsType {}
+interface StorePropsType { }
 
 /** FarmerSearch DispatchPropsType */
 interface DispatchPropsType {
@@ -48,7 +97,7 @@ interface DispatchPropsType {
 }
 
 /** FarmerSearch OwnStateType */
-interface OwnStateType {}
+interface OwnStateType { }
 
 /** FarmerSearch WrappedPropsType */
 type WrappedPropsType = StorePropsType & DispatchPropsType & OwnPropsType;
@@ -62,109 +111,99 @@ type StateType = OwnStateType;
 /** Farmer Search component for displaying and searching through farmers */
 class FarmerSearch extends React.Component<PropsType, StateType> {
 
-    public static defaultProps = {
-        listItems: defaultFarmerList,
-    };
+  /************************* Member Variables ************************/
 
-    /************************* Member Variables ************************/
+  private fuse: Fuse;
 
-    private searchBarValue = this.props.searchBarValue;
-    private farmerListFull = this.props.listItems;
+  /************************* Member Functions ************************/
 
-    /************************* Member Functions ************************/
+  public constructor(props: PropsType) {
+    super(props);
 
-    public constructor(props: PropsType) {
-        super(props);
+    // Initialization
+    // TODO: Uncomment this when using props for farmerList. Ask me about this if you need help James
+    // this.createFuse(props.farmerList);
 
-        this.renderItem = this.renderItem.bind(this);
-        this.itemClicked = this.itemClicked.bind(this);
-        this.searchList = this.searchList.bind(this);
-    }
+    // TODO: Delete this when you remove the global `farmerList` object
+    this.createFuse(farmerList);
 
-    /** Function to take user to farmer that was clicked on */
-    private itemClicked() {
-        this.props.navigateToFarmer();
-    }
-    /** Function to sort the list data by Farmer name in alphabetical order */
-    // TODO: Make required property
-    private sortList(farmers?: FarmerType[]): FarmerType[] {
+    // Bindings
+    this.renderItem = this.renderItem.bind(this);
+    this.onItemClick = this.onItemClick.bind(this);
+  }
 
-        var sortedList: FarmerType[] = [];
-        if (farmers === undefined) {
-            return sortedList;
-        }
-        sortedList = farmers.sort((f1: FarmerType, f2: FarmerType) => {
-            if (f1.name > f2.name) {
-                return 1;
-            }
-            if (f1.name < f2.name) {
-                return -1;
-            }
-            return 0;
-        });
-        return sortedList;
-    }
+  /** Click listener for list item */
+  private onItemClick() {
+    this.props.navigateToFarmer();
+  }
 
-    /** Function to perform a fuzzy search on the farmer list given a search term */
-    private searchList(farmers?: FarmerType[]): FarmerType[] {
-        var searchedList: FarmerType[] = [];
-        if (farmers === undefined) {
-            return searchedList;
-        }
-        var options = {
-            threshold: 0.6,
-            location: 0,
-            distance: 100,
-            maxPatternLength: 32,
-            minMatchCharLength: 1,
-            keys: [
-                'name',
-                'phoneNumber',
-            ]
-        };
-        var fuse = new Fuse(farmers, options); // "list" is the item array
-        if (this.searchBarValue === '') {
-            return farmers;
-        }
-        var result = fuse.search(this.searchBarValue);
-        this.forceUpdate();
+  private createFuse(farmerList: Farmer[]) {
+    this.fuse = new Fuse(farmerList, {
+      caseSensitive: false,
+      shouldSort: true,
+      threshold: 0.6,
+      location: 0,
+      distance: 100,
+      maxPatternLength: 32,
+      minMatchCharLength: 1,
+      keys: [
+        'name',
+        'phoneNumber',
+      ],
+    });
+  }
+
+  private searchList() {
+    return (this.props.searchBarValue) ?
+      this.fuse.search(this.props.searchBarValue) :
+      farmerList;
+  }
 
 
-        return result as FarmerType[];
-    }
+  /************************* React *************************/
 
+  public componentWillReceiveProps(nextProps: PropsType) {
+    // TODO: Uncomment when connecting to store for optimization @James
+    // Again ask if you have any questions
+    // if ((nextProps.farmerList.length !== this.props.farmerList.length)
+    //   || (nextProps.farmerList.every((val, i) => val === this.props.farmerList[i]))
+    // ) {
+    //   this.createFuse(nextProps.farmerList)
+    // }
+  }
 
-    /************************* React *************************/
+  // TODO: @nick, render should ideally always be at the bottom of the function
+  // Don't worry it takes some getting used to, I still forget all the time
+  public render(): JSX.Element {
+    return (
+      <Content style={style.background} >
+        <List
+          dataArray={this.searchList()}
+          renderRow={this.renderItem}
+        />
+      </Content>
+    );
+  }
 
-    public render(): JSX.Element {
-        return (
-            <Content style={style.background}>
-                <List
-                    dataArray={this.sortList(this.searchList(this.props.listItems))}
-                    renderRow={this.renderItem}
-                />
-
-            </Content>
-        );
-    }
-
-    /** Function to render the individual list items */
-    private renderItem(info: FarmerType) {
-        return (
-            <ListItem
-                key={info.id} onPress={this.itemClicked}
-            >
-                <View>
-                    <Text style={style.name}>
-                        {info.name}
-                    </Text>
-                    <Text style={style.phone}>
-                        Phone: {info.phoneNumber}
-                    </Text>
-                </View>
-            </ListItem>
-        );
-    }
+  // TODO: This is not a React lifecycle method and should be a member function
+  /** Function to render the individual list items */
+  private renderItem(info: Farmer) {
+    return (
+      <ListItem
+        key={info.id}
+        onPress={this.onItemClick}
+      >
+        <View>
+          <Text style={style.name}>
+            {info.name}
+          </Text>
+          <Text style={style.phone}>
+            Phone: {info.phoneNumber}
+          </Text>
+        </View>
+      </ListItem>
+    );
+  }
 }
 
 const FarmerSearchPage = createSearchPage<WrappedPropsType>(FarmerSearch, 'Search Farmers');
