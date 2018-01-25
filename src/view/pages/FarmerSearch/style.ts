dd0b085da48be0810cdf02077e40baa46ff30350
nickcassar <nscassar@ucalgary.ca>
2018-01-24 20:20:44 -0700

    AT-87A Hopefully this one works! Remade a new branch for cleaning up code and added only my changed files

---
+++ src/view/pages/FarmerSearch/style.ts
@@ -0,0 +1,14 @@
+/** Style sheet for Farmer Search */
+
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  name: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  phone: {
+    fontSize: 18,
+    fontWeight: 'normal',
+  },
+});
