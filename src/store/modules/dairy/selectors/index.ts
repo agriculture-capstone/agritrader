482097c5892d79546d46f979140eefafccdd0582
James Inglis <jamesekinglis@gmail.com>
2018-01-26 12:32:58 -0700

    I think these calculations will work need to be verified and cleaned up

--- src/store/modules/dairy/selectors/index.ts
+++ src/store/modules/dairy/selectors/index.ts
@@ -4,40 +4,53 @@ import UTCDate from '../../../../UTCDate'
 const getDairyEntries = state => state.dairy.dairyList;
 let getDate = entry => entry.datetime.dateString.split(' ').slice(0, 4).join(' ');
 
+/**Selector to calculate the current days milk collection */
 export const getDaysDairyTotal = createSelector(
   [getDairyEntries],
   dairyentries =>
     dairyentries.reducer((acc, entry) =>
-      acc + (getDate(entry) === getDate(getCurrentDate())) ? entry.volume : 0));
+      acc + (getDate(entry.datetime) === getDate(getCurrentDate())) ? entry.volume : 0));
 
+/**Selector to calculate the average daily milk collection **Still struggling with this one** */
 export const getAvgDaysDairyTotal = createSelector(
   [getDairyEntries],
   (dairyentries) => {
     const dayVolumeSums = dairyentries.map((entry) => {
       let days = [];
-      if (getDate(entry) in days) {
-        days[days.indexOf(getDate(entry)) += entry.volume];
+      if (getDate(entry.datetime) in days) {
+        days.find(x => x.id === getDate(entry.datetime)).value += entry.volume;//increment days array item with volume
       } else {
-        days.push({ getDate(entry): entry.volume });
+        days.push({ id: getDate(entry.datetime), value: entry.volume });
       }
       return days;
     });
-    let sum = 0;
-    // go through  dayVolumeSums array and sum all the values
+    return dayVolumeSums.reducer((acc, day) => acc + day.volume);
   },
 );
 
-
+/**Selector to calculate the farmers total milk collected this week **Still struggling with this one** */
 export const getWeeklyFarmerDairyTotal = createSelector(
   [getDairyEntries],
-  dairyentries =>
-    dairyentries.reducer((acc, entry) =>
-      acc + (getDate(entry) === getDate(getCurrentDate())) ? entry.volume : 0));
+  (dairyentries) => {
+    let weekSum: number = 0;
+    let weekDay: number = getCurrentDate().getDay;
+    dairyentries.forEach(entry => {
+      for (let i = 0; i <= weekDay; i++) {
+        if (convertToDate(entry.datetime) === getCurrentDate().setDate(today.getDate() - i)) {
+          weekSum += entry.volume;
+        } else {
+          continue;
+        }
+      }
+    });
+  },
+);
 
+/**Selector to calculate the farmers total milk collected this month */
 export const getMonthlyFarmerDairyTotal = createSelector(
   [getDairyEntries],
   dairyentries =>
     dairyentries.reducer((acc, entry) =>
-      acc + ((convertToDate(entry.dateTime).getMonth() === getCurrentDate().getMonth) 
-      && (convertToDate(entry.dateTime).getYear() === getCurrentDate().getYear)) ? entry.volume : 0));
+      acc + ((convertToDate(entry.dateTime).getMonth() === getCurrentDate().getMonth)
+        && (convertToDate(entry.dateTime).getYear() === getCurrentDate().getYear)) ? entry.volume : 0));
 
