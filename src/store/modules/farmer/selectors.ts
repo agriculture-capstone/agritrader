2d32273883af502f675fbfb9409cc0e5494f04a6
James Inglis <jamesekinglis@gmail.com>
2018-01-28 14:31:09 -0700

    changed some comments

--- src/store/modules/farmer/selectors.ts
+++ src/store/modules/farmer/selectors.ts
@@ -3,11 +3,11 @@ import { Farmer } from './types';
 import { State } from '../../types';
 
 const getFarmerEntries = (state: State) => state.farmer.rows;
-// const getCurrentFarmerUUID = (state: State) => state.currentFarmer.currentFarmerUUID; //need to merge with dairy/master for this
+const getCurrentFarmerUUID = (state: State) => state.currentFarmer.currentFarmerUUID;
 
 /************Selectors for a specific farmer ***************/
 
-/**Selector for a subset of fields for all farmers (used on search page) */
+/**Selector for a subset of fields for all farmers (not sure if we need this but if a page takes an array we might) */
 export const getFormattedFarmers = createSelector(
   [getFarmerEntries],
   (farmEntries: Farmer[]) => farmEntries.map(entry => 
@@ -15,7 +15,7 @@ export const getFormattedFarmers = createSelector(
   ),
 );
 
-/**Selector to obtain a specific farmers info */// need to merge with dairy/master for this
-// export const getSpecificFarmer = createSelector(
-//   [getFarmerEntries, getCurrentFarmerUUID],
-//   (farmerEntries: Farmer[], farmerUUID: string) => farmerEntries.filter(entry => !entry.uuid.localeCompare(farmerUUID)));
+/**Selector to obtain a specific farmers info */
+export const getSpecificFarmer = createSelector(
+  [getFarmerEntries, getCurrentFarmerUUID],
+  (farmerEntries: Farmer[], farmerUUID: string) => farmerEntries.filter(entry => !entry.uuid.localeCompare(farmerUUID)));
