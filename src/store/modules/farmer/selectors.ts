691b9bfcc3c8ecce8c5104e17fb9fb07d75543e3
James Inglis <jamesekinglis@gmail.com>
2018-01-29 00:06:00 -0700

    removed unneeded selector

--- src/store/modules/farmer/selectors.ts
+++ src/store/modules/farmer/selectors.ts
@@ -7,14 +7,6 @@ const getCurrentFarmerUUID = (state: State) => state.currentFarmer.currentFarmer
 
 /************Selectors for a specific farmer ***************/
 
-/**Selector for a subset of fields for all farmers (not sure if we need this but if a page takes an array we might) */
-export const getFormattedFarmers = createSelector(
-  [getFarmerEntries],
-  (farmEntries: Farmer[]) => farmEntries.map(entry => 
-    ({ name: (entry.firstName + ' ' + entry.lastName), phoneNumber:('(' + entry.phoneArea + ') ' + entry.phoneNumber), uuid:entry.uuid }), 
-  ),
-);
-
 /**Selector to obtain a specific farmers info */
 export const getSpecificFarmer = createSelector(
   [getFarmerEntries, getCurrentFarmerUUID],
