092124b3e58eb17943c6c2896d0e495fb81df10c
Brad <bradpf@outlook.com>
2018-02-07 17:14:30 -0700

    AT-197 Removing reflection in custom errors

--- src/errors/NetworkError.ts
+++ src/errors/NetworkError.ts
@@ -4,6 +4,9 @@ const DEFAULT_MSG = 'Failed to make request due to poor network conditions';
 
 /** Error thrown when network connection is expensive or absent */
 export class NetworkError extends BaseError {
+
+  public static id = 'NetworkError';
+
   constructor(msg = DEFAULT_MSG) {
     super(NetworkError, msg);
   }
