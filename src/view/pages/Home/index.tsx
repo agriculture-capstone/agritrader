e50c5b68b3d9511aee087003f0f59165cdb90ae8
jeinglis <jamesekinglis@gmail.com>
2018-02-16 04:02:21 -0700

    change prop name

--- src/view/pages/Home/index.tsx
+++ src/view/pages/Home/index.tsx
@@ -9,7 +9,7 @@ import StatisicsBlock from '../../components/StatisticsBlock';
 import { Route } from '../../navigation/routes';
 import { State } from '../../../store/types';
 import navActions from '../../../store/modules/nav/actions';
-import { getAvgDaysMilkTotal, getDaysMilkTotal } from '../../../store/modules/milk/selectors';
+import { getAvgDaysMilkTotal, getDaysMilkTotal, getMilkInventory } from '../../../store/modules/milk/selectors';
 import Composer from '../../hoc/PageComposer';
 import styles from './style';
 import * as moment from 'moment';
@@ -19,7 +19,7 @@ interface OwnPropsType { }
 
 interface StorePropsType {
   dayTotal: string;
-  avgDayTotal: string;
+  milkInventory: string;
 }
 
 interface DispatchPropsType {
@@ -64,7 +64,7 @@ class Home extends React.Component<PropsType, {}> {
             </Row>
             <Row>
               <StatisicsBlock value={this.props.dayTotal} units="L" label="Collected Today" />
-              <StatisicsBlock value={this.props.avgDayTotal} units="L" label="Milk Inventory" />
+              <StatisicsBlock value={this.props.milkInventory} units="L" label="Milk Inventory" />
             </Row>
           </Grid>
         </Panel>
@@ -84,6 +84,7 @@ const mapStateToProps: MapStateToProps<StorePropsType, OwnPropsType, State> = (s
   return {
     dayTotal: getDaysMilkTotal(state),
     avgDayTotal: getAvgDaysMilkTotal(state),
+    milkInventory: getMilkInventory(state),
   };
 };
 
