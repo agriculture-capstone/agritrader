044a5b36214b998f66a9a7efe1c5a6787b5e0a85
nickcassar <nscassar@ucalgary.ca>
2018-03-24 17:11:37 -0600

    AT-240 fixed back stack navigation

--- src/view/components/PaymentEntryDetails/index.tsx
+++ src/view/components/PaymentEntryDetails/index.tsx
@@ -147,7 +147,7 @@ const mapStateToProps: MapStateToProps<StorePropsType, OwnPropsType, State> = (s
 
 const mapDispatchToProps: MapDispatchToProps<DispatchPropsType, OwnPropsType> = (dispatch) => {
   return {
-    navigate: (route: Route) => dispatch(navActions.navigateTo(route)),
+    navigate: (route: Route) => dispatch(navActions.navigateToWithoutHistory(route)),
   };
 };
 
