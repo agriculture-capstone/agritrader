4cf670bd205165d19f2727c22d910be8c5bdac8c
nickcassar <nscassar@ucalgary.ca>
2018-03-24 17:28:51 -0600

    AT-240 Added number formatter to Farmer total balance selector

--- src/store/selectors.ts
+++ src/store/selectors.ts
@@ -4,6 +4,8 @@ import { getFarmerDairyBalanceNoFormat as getFarmerDairyBalance } from './module
 import { getFarmerLoanBalance } from './modules/loan/selectors';
 import { getFarmerPaymentBalance } from './modules/payment/selectors';
 
+const decimals = 1;
+
 /**
  * Selector for the farmer total balance for the week
  * Includes payments, loans, and dairy transactions
@@ -11,5 +13,26 @@ import { getFarmerPaymentBalance } from './modules/payment/selectors';
 export const getFarmerTotalBalance = createSelector(
   [getFarmerPaymentBalance, getFarmerLoanBalance, getFarmerDairyBalance],
   (paymentBalance: string, loanBalance: string, dairyBalance: string) => 
-  ((Number(dairyBalance) - Number(paymentBalance) - Number(loanBalance)).toString()),
+  numberFormatter((Number(dairyBalance) - Number(paymentBalance) - Number(loanBalance))).toString(),
 );
+
+/**
+ * Function to format large numbers to be more readable
+ * 
+ * @param num Number that will be formatted
+ */
+function numberFormatter(num: number) {
+  const thousand = 1000;
+  const million = 1000000;
+  const billion = 1000000000;
+  if (Math.abs(num) >= billion) {
+    return (num / billion).toFixed(decimals).replace(/\.0$/, '') + 'B';
+  }
+  if (Math.abs(num) >= million) {
+    return (num / million).toFixed(decimals).replace(/\.0$/, '') + 'M';
+  }
+  if (Math.abs(num) >= thousand) {
+    return (num / thousand).toFixed(decimals).replace(/\.0$/, '') + 'K';
+  }
+  return num;
+}
